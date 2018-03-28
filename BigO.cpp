1dfb95847435657299e2542a74131f76d3785a92
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-03-28 17:36:49 -0400

    Create BigO.cpp

---
+++ BigO.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+int main(){
+
+
+vector <int> data;
+int length = 10;
+// load the data in vector
+int min=0;
+int max=0;
+for (int i =0; i < 10 ; i++)
+{
+  data.push_back(rand() % 5);
+}
+
+auto start = chrono::high_resolution_clock::now()
+
+for(int i=0, min = data.at(0), max = data.at(0);i<length;i++)
+{
+  if (data.at(i) < min)   min = data.at(i);
+  if (data.at(i) > max)   max = data.at(i);
+}
+cout << "max: " << max << " min: " << min; 
+
+auto end = chrono::high_resolution_clock::now()
+chrono::duration<double> el = end - start;
+cout<<"Execution time is:     " << el.count() * 1000 << " mS " << endl;
+
+
+retrun 0;
+}
