d9d451573bf8f6768a3ef534835ce5c7b1f17a06
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-03-31 16:45:52 -0400

    Update scriptDownload.sh

--- scriptDownload.sh
+++ scriptDownload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git pull origin master
+# git pull origin master
 # git config credential.helper store
 g++ pg41ex2.cpp -std=c++11 -o pg41ex2.exe
 g++ pg46ex1.cpp -std=c++11 -o pg46ex1.exe
