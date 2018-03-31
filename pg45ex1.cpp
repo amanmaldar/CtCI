2e9e73865b3654382e030012ce79be8ffb23b367
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-03-31 17:05:59 -0400

    Create pg45ex1.cpp

---
+++ pg45ex1.cpp
@@ -0,0 +1,29 @@
+// This is recursion example
+
+#include <iostream>
+#include <chrono>
+
+#include<time.h>
+#include <Windows.h>
+using namespace std;
+
+
+int f(int n) {
+if (n <= 1) {
+  if (n <= 1) { return 1; }
+  Sleep(1000);
+  return f(n - 1) + f(n - 1);
+}
+
+int main(){
+
+
+
+auto start = chrono::high_resolution_clock::now();
+
+f(2);
+auto end = chrono::high_resolution_clock::now();
+ chrono::duration<double> el = end - start;
+    
+return 1;
+}
