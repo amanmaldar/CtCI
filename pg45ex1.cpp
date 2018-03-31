a31070df39064548d25221bc964417ca0e05057f
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-03-31 17:16:06 -0400

    Update pg45ex1.cpp

--- pg45ex1.cpp
+++ pg45ex1.cpp
@@ -16,10 +16,21 @@ void numberFunction(int i) {
   }
 }
 
+int fib(int x) {
+    if (x == 0)
+        return 0;
+
+    if (x == 1)
+        return 1;
+
+    return fib(x-1)+fib(x-2);
+}
+
 int main() {
 
 int i = 0;
-numberFunction(i);
+//numberFunction(i);
+cout <<  "ans: " << fib(5) << endl; 
 
 return 0;
 }
