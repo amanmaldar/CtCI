e1fb40ac12c65fda79d4dbcf8d47df637198649a
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-03-31 17:04:29 -0400

    Update scriptDownload.sh

--- scriptDownload.sh
+++ scriptDownload.sh
@@ -2,6 +2,9 @@
 # if changes are made to this script, run the "git pull origin master" seperately once
 git pull origin master
 # git config credential.helper store
-g++ pg41ex2.cpp -std=c++11 -o pg41ex2.exe
-g++ pg46ex1.cpp -std=c++11 -o pg46ex1.exe
+#g++ pg41ex2.cpp -std=c++11 -o pg41ex2.exe
+#g++ pg46ex1.cpp -std=c++11 -o pg46ex1.exe
+g++ pg45ex1.cpp -std=c++11 -o pg45ex1.exe
+
+./pg45ex1.exe
 #
