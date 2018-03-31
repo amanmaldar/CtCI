3d334eba51114fa7cce299840e4a86b4c70bdecb
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-03-31 18:40:15 -0400

    Update pg45ex1.cpp

--- pg45ex1.cpp
+++ pg45ex1.cpp
@@ -1,4 +1,5 @@
-// This is recursion example
+ // This is recursion example fibonacci 1 1 2 3 5 8 13
+// ref - https://www.youtube.com/watch?v=zg-ddPbzcKM
 
 #include <iostream>
 #include <chrono>
@@ -17,20 +18,22 @@ void numberFunction(int i) {
 }
 
 int fib(int x) {
-    if (x == 0)
+ /*   if (x == 0)   // if my series starts with 1, return 1. See following if condition
         return 0;
 
     if (x == 1)
-        return 1;
-
-    return fib(x-1)+fib(x-2);
+        return 1; */
+    if (x < 2)
+      return 1;
+    else
+      return fib(x-1)+fib(x-2);
 }
 
 int main() {
 
 int i = 0;
 //numberFunction(i);
-cout <<  "ans: " << fib(10) << endl; 
+cout <<  "ans: " << fib(5) << endl; 
 
 return 0;
 }
