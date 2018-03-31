cf4b4d87607dda32fcc9c6adeea3f667e2f505ec
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-03-30 21:40:36 -0400

    Update scriptDownload.sh

--- scriptDownload.sh
+++ scriptDownload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull origin master
-#g++ BigO.cpp -std=c++11 -o BigO.exe
-g++ BigO_1.cpp -std=c++11 -o BigO_1.exe
+g++ BigO.cpp -std=c++11 -o BigO.exe
+g++ pg46ex1.cpp -std=c++11 -o pg46ex1.exe
 #./BigO.exe
-./BigO_1.exe
+./pg46ex1.exe
 #
